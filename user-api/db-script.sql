f6e0dae6d0f443becf23f8a22a442dd874d3736a
Akash Gupta <guptaakashiitg@gmail.com>
2020-10-20 08:16:56 +0530

    minor change

--- user-api/db-script.sql
+++ user-api/db-script.sql
@@ -1,3 +1,4 @@
+CREATE SCHEMA `benchms` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 create table user(id tinyint unsigned primary key auto_increment,
 username varchar(32),
 email varchar(64),
